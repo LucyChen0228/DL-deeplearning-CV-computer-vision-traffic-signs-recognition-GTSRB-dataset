5d309c87cc4ab3f3d8605b5d45a5ed86e19f7676
Lucy <42339395+LucyChen0228@users.noreply.github.com>
2018-11-22 22:48:09 +0800

    Add files via upload

---
+++ VGG16模型预测试.py
@@ -0,0 +1,14 @@
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+model = VGG16(weights='imagenet', include_top=False)
+
+img_path = 'elephant_imagenet.jpg'  #再次提醒自己：记得把图片放到文件夹下
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+features_predicted = model.predict(x)
